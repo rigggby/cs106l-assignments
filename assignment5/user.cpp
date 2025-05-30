e867585966f4da6528d26ef21b08922895714d00
rigggby <lmhhhml2@gmail.com>
2025-05-30 21:10:03 +0800

    assignment 5

--- assignment5/user.cpp
+++ assignment5/user.cpp
@@ -11,6 +11,35 @@ User::User(const std::string& name)
 {
 }
 
+User::User(const User& other)
+  : _name(other.get_name())
+  , _friends(new std::string[other.capacity()])
+  , _size(other.size())
+  , _capacity(other.capacity()) 
+{
+  for (size_t i = 0; i < other.size(); ++i) {
+    _friends[i] = other._friends[i];
+  }
+}
+
+User& User::operator=(const User& other) {
+  if (this == &other) return *this;
+  this->_name = other.get_name();
+  delete[] this->_friends;
+  this->_friends = new std::string[other.capacity()];
+  this->_size = other.size();
+  this->_capacity = other.capacity();
+  for (size_t i = 0; i < other.size(); ++i) {
+    this->_friends[i] = other._friends[i];
+  }
+  return *this;
+}
+
+User::~User()
+{
+  delete[] _friends;
+}
+
 /**
  * Adds a friend to this User's list of friends.
  * @param name The name of the friend to add.
@@ -49,6 +78,10 @@ User::size() const
   return _size;
 }
 
+size_t User::capacity() const {
+  return _capacity;
+}
+
 /**
  * Sets the friend at the given index to the given name.
  * @param index The index of the friend to set.
@@ -59,7 +92,24 @@ void User::set_friend(size_t index, const std::string& name)
   _friends[index] = name;
 }
 
-/** 
- * STUDENT TODO:
- * The definitions for your custom operators and special member functions will go here!
- */
+std::ostream& operator<<(std::ostream& os, const User& user)
+{
+  os << "User(name=" << user.get_name() << ", friends=[";
+  for (size_t i = 0; i < user.size(); ++i) {
+    if (i > 0) os << ", ";
+    os << user._friends[i];
+  }
+  os << "])";
+  return os;
+}
+
+User& User::operator+=(User& rhs) {
+  if (this == &rhs) return *this;
+  this->add_friend(rhs.get_name());
+  rhs.add_friend(this->get_name());
+  return *this;
+}
+
+bool User::operator<(const User& rhs) const {
+  return this->get_name() < rhs.get_name();
+}
--- assignment5/user.h
+++ assignment5/user.h
@@ -10,15 +10,19 @@ class User
 {
 public:
   User(const std::string& name);
+  User(const User& other);
+  User& operator=(const User& other);
+  User(User&& other) = delete;
+  User& operator=(User&& other) = delete;
+  User& operator+=(User& rhs);
+  bool operator<(const User& rhs) const;
+  ~User();
   void add_friend(const std::string& name);
   std::string get_name() const;
   size_t size() const;
+  size_t capacity() const;
   void set_friend(size_t index, const std::string& name);
-
-  /** 
-   * STUDENT TODO:
-   * Your custom operators and special member functions will go here!
-   */
+  friend std::ostream& operator<<(std::ostream& os, const User& user);
 
 private:
   std::string _name;
